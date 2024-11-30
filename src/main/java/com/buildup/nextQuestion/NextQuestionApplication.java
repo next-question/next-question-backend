aece1222745de0b2ce800108ee8fefde0ba9cb1b
juns0720 <juns0720@naver.com>
2024-11-30 17:12:55 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/buildup/nextQuestion/NextQuestionApplication.java
@@ -0,0 +1,13 @@
+package com.buildup.nextQuestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextQuestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildup/nextQuestion/NextQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildup.nextQuestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextQuestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
