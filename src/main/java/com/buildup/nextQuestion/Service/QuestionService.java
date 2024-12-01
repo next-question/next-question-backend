301968b755017b3c7f3ac248571b3a4235975437
dupong00 <tjdgns9954@gmail.com>
2024-12-01 23:28:19 +0900

    fix : QuestionService 수정 단일 저장/업데이트 -> 다중 저장/업데이트로 통합

--- src/main/java/com/buildup/nextQuestion/Service/QuestionService.java
+++ src/main/java/com/buildup/nextQuestion/Service/QuestionService.java
@@ -15,11 +15,6 @@ public class QuestionService {
     @Autowired
     private QuestionRepository questionRepository;
 
-    //생성된 문제 저장(문제 db에 저장)
-    public void save(Question question) {
-        questionRepository.save(question);
-    }
-
     //생성된 문제 리스트 저장
     public void saveAll(List<Question> questions) {questionRepository.saveAll(questions);}
 
@@ -28,18 +23,8 @@ public class QuestionService {
         return questionRepository.findAllByWorkBookId(workbook_id);
     }
 
-    //문제 정보 갱신(받아온거 sql update)
-    public void questionUpdate(QuestionUpdateRequest updatedQuestion) {
-        Long questionId = updatedQuestion.getQuestionId();
-
-        Question existingQuestion = questionRepository.findById(questionId).get(); //id로 Question객체 가져옴
-        existingQuestion.setWrong(updatedQuestion.getWrong()); //객체 정답여부 update
-        existingQuestion.setRecentSolveTime(updatedQuestion.getRecentSolveTime()); //객체 최근 시간 update
-
-        questionRepository.save(existingQuestion); //객체 저장
-    }
-
-    public void questionsUpdate(List<QuestionUpdateRequest> updatedQuestions) {
+    //문제 정보 갱신(update)
+    public void questionUpdate(List<QuestionUpdateRequest> updatedQuestions) {
         for(QuestionUpdateRequest request : updatedQuestions) {
             Long questionId = request.getQuestionId();
 
