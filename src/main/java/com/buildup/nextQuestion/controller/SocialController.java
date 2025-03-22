5fa2c148aed5382de2ee6ebc9e73f3c099d9e8cb
Dupong00 <tjdgns9954@gmail.com>
2025-03-22 14:32:51 +0900

    feature/googleLogin : 구글 회원가입/로그인 기능 추가

---
+++ src/main/java/com/buildup/nextQuestion/controller/SocialController.java
@@ -0,0 +1,45 @@
+package com.buildup.nextQuestion.controller;
+
+import com.buildup.nextQuestion.dto.member.LoginResponse;
+import com.buildup.nextQuestion.dto.member.RegistRequest;
+import com.buildup.nextQuestion.repository.SocialMemberRepository;
+import com.buildup.nextQuestion.service.MemberService;
+import com.buildup.nextQuestion.service.SocialMemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class SocialController {
+
+    private final SocialMemberRepository socialMemberRepository;
+    private final SocialMemberService socialMemberService;
+
+    //구글 로그인 페이지 반환
+    @PostMapping("public/oauth2/google")
+    public String postGooglePage() {
+        String reqUrl = socialMemberService.loginUrlGoogle();
+        return reqUrl;
+    }
+
+    // 구글 인증 후 받은 authCode를 처리하는 엔드포인트
+    @GetMapping("public/oauth2/google")
+    public ResponseEntity<?> login(@RequestParam("code") String authCode) {
+        String snsId = socialMemberService.authGoogle(authCode);  // authCode로 snsId 추출
+        if (socialMemberRepository.existsBySnsId(snsId)) {
+            LoginResponse response = socialMemberService.loginGoogle(snsId);
+            return ResponseEntity.ok(response);  // 로그인 응답 반환
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("아이디가 존재하지 않습니다. 회원가입을 진행해 주세요.");
+        }
+    }
+
+    @PostMapping("public/member/regist/social/google")
+    public ResponseEntity<?> register(@RequestBody RegistRequest registDTORequest) {
+        socialMemberService.registerGoogle(registDTORequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입에 성공했습니다.");
+    }
+}
---
+++ src/main/java/com/buildup/nextQuestion/service/SocialMemberService.java
@@ -0,0 +1,91 @@
+package com.buildup.nextQuestion.service;
+
+import com.buildup.nextQuestion.domain.Member;
+import com.buildup.nextQuestion.domain.SocialMember;
+import com.buildup.nextQuestion.domain.enums.LoginType;
+import com.buildup.nextQuestion.domain.enums.SocialType;
+import com.buildup.nextQuestion.dto.member.LoginResponse;
+import com.buildup.nextQuestion.dto.member.RegistRequest;
+import com.buildup.nextQuestion.dto.google.GoogleInfResponse;
+import com.buildup.nextQuestion.dto.google.GoogleRequest;
+import com.buildup.nextQuestion.dto.google.GoogleResponse;
+import com.buildup.nextQuestion.repository.MemberRepository;
+import com.buildup.nextQuestion.repository.SocialMemberRepository;
+import com.buildup.nextQuestion.utility.JwtUtility;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SocialMemberService {
+    private final SocialMemberRepository socialMemberRepository;
+    private final JwtUtility jwtUtility;
+    private final MemberRepository memberRepository;
+
+    @Value("${google.client.id}")
+    private String googleClientId;
+
+    @Value("${google.client.pw}")
+    private String googleClientPw;
+
+    public String loginUrlGoogle(){
+        String reqUrl = "https://accounts.google.com/o/oauth2/v2/auth?client_id=" + googleClientId
+                + "&redirect_uri=http://localhost:8080/public/oauth2/google&response_type=code&scope=email%20profile%20openid&access_type=offline&prompt=consent";
+        return reqUrl;
+    }
+
+    public String authGoogle(String authCode) {
+        RestTemplate restTemplate = new RestTemplate();
+        GoogleRequest googleOAuthRequestParam = GoogleRequest
+                .builder()
+                .clientId(googleClientId)
+                .clientSecret(googleClientPw)
+                .code(authCode)
+                .redirectUri("http://localhost:8080/public/oauth2/google")
+                .grantType("authorization_code").build();
+        ResponseEntity<GoogleResponse> resultEntity = restTemplate.postForEntity("https://oauth2.googleapis.com/token",
+                googleOAuthRequestParam, GoogleResponse.class);
+        String jwtToken = resultEntity.getBody().getId_token();
+        Map<String, String> map = new HashMap<>();
+        map.put("id_token", jwtToken);
+        ResponseEntity<GoogleInfResponse> resultEntity2 = restTemplate.postForEntity("https://oauth2.googleapis.com/tokeninfo",
+                map, GoogleInfResponse.class);
+
+        String snsId = resultEntity2.getBody().getEmail();
+        return snsId;
+    }
+
+    public LoginResponse loginGoogle(String snsId) {
+        SocialMember socialMember = socialMemberRepository.findBySnsId(snsId).orElseThrow(() -> new IllegalArgumentException("해당 ID가 존재하지 않습니다."));
+        Member member = socialMember.getMember();
+        LoginResponse loginDTOresponse = new LoginResponse(jwtUtility.generateToken(snsId, member.getRole()), member.getNickname(), member.getRole());
+        // 3. JWT 토큰 생성 후 반환
+        return loginDTOresponse;
+    }
+
+    @Transactional
+    public void registerGoogle(RegistRequest registDTORequest) {
+        String userId = registDTORequest.getUserId();
+        String nickname = registDTORequest.getNickname();
+        // 중복 체크
+        if (memberRepository.existsByNickname(nickname)) {
+            throw new IllegalArgumentException("동일한 별명을 사용할 수 없습니다.");
+        }
+
+        // Member 객체 생성
+        Member member = new Member(nickname, LoginType.SOCIAL);
+        memberRepository.save(member);
+
+        SocialMember socialMember = new SocialMember(userId, SocialType.GOOGLE, member);
+        socialMemberRepository.save(socialMember);
+    }
+}
