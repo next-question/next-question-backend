fa12ba84e15f95f7c75a77cf343a314df0349f56
Seopyo <seofio@naver.com>
2025-05-10 20:29:06 +0900

    refactor/follow

--- src/main/java/com/buildup/nextQuestion/repository/HistoryRepository.java
+++ src/main/java/com/buildup/nextQuestion/repository/HistoryRepository.java
@@ -2,16 +2,14 @@ package com.buildup.nextQuestion.repository;
 
 import com.buildup.nextQuestion.domain.History;
 import com.buildup.nextQuestion.domain.Member;
-import com.buildup.nextQuestion.domain.enums.SolvedType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
     List<History> findAllByMemberId(Long id);
     List<History> findAllByMemberIdAndSolvedDateBetween(Long memberId, Timestamp startDate, Timestamp endDate);
     List<History> findByMemberOrderBySolvedDateAsc(Member member);
+    List<History> findByMemberIdAndSolvedDateAfter(Long memberId, Timestamp fromDate);
 }
