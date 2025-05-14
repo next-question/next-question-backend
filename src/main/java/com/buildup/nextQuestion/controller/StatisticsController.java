4180d38a8173a67616a399d5291e1a61a6b677c6
Seopyo27 <134706663+Seopyo27@users.noreply.github.com>
2025-05-14 11:50:41 +0900

    Revert "[feature] 최근 푼, 틀린, 많이 틀린 문제 조회 기능 추가"

--- src/main/java/com/buildup/nextQuestion/controller/StatisticsController.java
+++ src/main/java/com/buildup/nextQuestion/controller/StatisticsController.java
@@ -1,13 +1,14 @@
 package com.buildup.nextQuestion.controller;
 
-import com.buildup.nextQuestion.dto.question.MemberQuestionInfoDto;
 import com.buildup.nextQuestion.dto.statistics.DayQuestionStats;
-import com.buildup.nextQuestion.dto.statistics.QuestionStatisticsRequest;
 import com.buildup.nextQuestion.service.SolvingService;
 import com.buildup.nextQuestion.service.StatisticsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -24,35 +25,5 @@ public class StatisticsController {
     ) {
         List<DayQuestionStats> stats = statisticsService.findCorrectQuestions(token);
         return ResponseEntity.ok(stats);
-    }
-
-    // 1. 푼 문제 목록 조회
-    @GetMapping("/solved")
-    public ResponseEntity<List<MemberQuestionInfoDto>> getSolvedQuestions(
-            @RequestHeader("Authorization") String token,
-            @RequestBody QuestionStatisticsRequest request) {
-
-        List<MemberQuestionInfoDto> result = statisticsService.getSolvedQuestion(token, request);
-        return ResponseEntity.ok(result);
-    }
-
-    // 2. 틀린 문제 목록 조회
-    @GetMapping("/wrong")
-    public ResponseEntity<List<MemberQuestionInfoDto>> getWrongQuestions(
-            @RequestHeader("Authorization") String token,
-            @RequestBody QuestionStatisticsRequest request) {
-
-        List<MemberQuestionInfoDto> result = statisticsService.getWrongQuestion(token, request);
-        return ResponseEntity.ok(result);
-    }
-
-    // 3. 자주 틀린 문제 목록 조회
-    @GetMapping("/wrong-frequent")
-    public ResponseEntity<List<MemberQuestionInfoDto>> getFrequentlyWrongQuestions(
-            @RequestHeader("Authorization") String token,
-            @RequestBody QuestionStatisticsRequest request) {
-
-        List<MemberQuestionInfoDto> result = statisticsService.getFrequentlyWrongQuestion(token, request);
-        return ResponseEntity.ok(result);
     }
 }
