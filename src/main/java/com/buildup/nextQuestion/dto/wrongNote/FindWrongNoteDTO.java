e0a03d37b20b2f1fd4d643a303afe24b846bf873
Dupong00 <tjdgns9954@gmail.com>
2025-05-17 16:46:27 +0900

    fix : 오답노트 전 버전 응답으로 변경

---
+++ src/main/java/com/buildup/nextQuestion/dto/wrongNote/FindWrongNoteDTO.java
@@ -0,0 +1,24 @@
+package com.buildup.nextQuestion.dto.wrongNote;
+
+import com.buildup.nextQuestion.domain.enums.QuestionType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FindWrongNoteDTO {
+    private String encryptedQuestionId;
+    private String encryptedWorkBookId;
+    private String workBookName;
+    private String name;
+    private QuestionType type;
+    private String answer;
+    private String opt;
+    private Timestamp solvedDate;
+    private Timestamp recentSolveTime;
+}
+
